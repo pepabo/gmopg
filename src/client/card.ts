fc03a924432da87be101982f543d82c9c310519e
linyows <linyows@gmail.com>
2017-10-02 18:14:17 +0900

    simplize card client

--- src/client/card.ts
+++ src/client/card.ts
@@ -1,145 +1,97 @@
-import axios, { AxiosResponse } from 'axios'
-import * as querystring from 'querystring'
+import { AxiosResponse } from 'axios'
+import * as qs from 'querystring'
+import Client, { ISiteArgs } from '../client'
 import * as util from '../util'
 
-export interface ISaveCardArgs {
-  siteID: string
-  sitePass: string
-  memberID: string
-  seqMode?: util.SeqMode
-  cardSeq?: number
-  defaultFlag?: string
-  cardName?: string
-  cardNo?: string
-  cardPass?: string
-  expire?: string
-  holderName?: string
-  token?: string
+export interface ISaveCardArgs extends ISiteArgs {
+  SeqMode?: util.SeqMode
+  CardSeq?: number
+  DefaultFlag?: string
+  CardName?: string
+  CardNo?: string
+  CardPass?: string
+  Expire?: string
+  HolderName?: string
+  Token?: string
 }
 
 export interface ISaveCardResult {
-  cardSeq: string
-  cardNo: string
-  forward: string
-  brand?: string
-  domesticFlag?: string
-  issuerCode?: string
-  debitPrepaidFlag?: string
-  debitPrepaidIssuerName?: string
-  forwardFinal?: string
+  CardSeq: string
+  CardNo: string
+  Forward: string
+  Brand?: string
+  DomesticFlag?: string
+  IssuerCode?: string
+  DebitPrepaidFlag?: string
+  DebitPrepaidIssuerName?: string
+  ForwardFinal?: string
 }
 
-export async function saveCard(args: ISaveCardArgs): Promise<ISaveCardResult> {
-  const res: AxiosResponse = await axios.post(`${process.env.GMOPG_ENDPOINT}/payment/SaveCard.idPass`, {
-    SiteID: args.siteID,
-    SitePass: args.sitePass,
-    MemberID: args.memberID,
-    SeqMode: args.seqMode,
-    CardSeq: args.cardSeq,
-    DefaultFlag: args.defaultFlag,
-    CardName: args.cardName,
-    CardNo: args.cardNo,
-    CardPass: args.cardPass,
-    Expire: args.expire,
-    HolderName: args.holderName,
-    Token: args.token
-  })
-
-  const result = querystring.parse(res.data)
-
-  return {
-    cardSeq: result.CardSeq,
-    cardNo: result.CardNo,
-    forward: result.Forward,
-    brand: result.Brand,
-    domesticFlag: result.DomesticFlag,
-    issuerCode: result.IssuerCode,
-    debitPrepaidFlag: result.DebitPrepaidFlag,
-    debitPrepaidIssuerName: result.DebitPrepaidIssuerName,
-    forwardFinal: result.ForwardFinal
-  }
-}
-
-export interface IDeleteCardArgs {
-  siteID: string
-  sitePass: string
-  memberID: string
-  seqMode?: util.SeqMode
-  cardSeq: string
+export interface IDeleteCardArgs extends ISiteArgs {
+  SeqMode?: util.SeqMode
+  CardSeq: string
 }
 
 export interface IDeleteCardResult {
-  cardSeq: string
+  CardSeq: string
 }
 
-export async function deleteCard(args: IDeleteCardArgs): Promise<IDeleteCardResult> {
-  const res: AxiosResponse = await axios.post(`${process.env.GMOPG_ENDPOINT}/payment/DeleteCard.idPass`, {
-    SiteID: args.siteID,
-    SitePass: args.sitePass,
-    MemberID: args.memberID,
-    SeqMode: args.seqMode,
-    CardSeq: args.cardSeq
-  })
-
-  const result = querystring.parse(res.data)
-
-  return {
-    cardSeq: result.CardSeq
-  }
-}
-
-export interface ISearchCardArgs {
-  siteID: string
-  sitePass: string
-  memberID: string
-  seqMode: util.SeqMode
-  cardSeq?: string
+export interface ISearchCardArgs extends ISiteArgs {
+  SeqMode: util.SeqMode
+  CardSeq?: string
 }
 
 export interface ISearchCardResult {
-  cardSeq: string
-  defaultFlag: string
-  cardName: string
-  cardNo: string
-  expire: string
-  holderName: string
-  deleteFlag: string
-  brand?: string
-  domesticFlag?: string
-  issuerCode?: string
-  debitPrepaidFlag?: string
-  debitPrepaidIssuerName?: string
-  forwardFinal?: string
+  CardSeq: string
+  DefaultFlag: string
+  CardName: string
+  CardNo: string
+  Expire: string
+  HolderName: string
+  DeleteFlag: string
+  Brand?: string
+  DomesticFlag?: string
+  IssuerCode?: string
+  DebitPrepaidFlag?: string
+  DebitPrepaidIssuerName?: string
+  ForwardFinal?: string
 }
 
-export async function searchCard(args: ISearchCardArgs): Promise<ISearchCardResult[]> {
-  const res: AxiosResponse = await axios.post(`${process.env.GMOPG_ENDPOINT}/payment/SearchCard.idPass`, {
-    SiteID: args.siteID,
-    SitePass: args.sitePass,
-    MemberID: args.memberID,
-    SeqMode: args.seqMode,
-    CardSeq: args.cardSeq
-  })
+export default class Card extends Client {
+  public async save(args: ISaveCardArgs): Promise<ISaveCardResult> {
+    const res: AxiosResponse = await this.client.post('/payment/SaveCard.idPass', args)
 
-  const result = querystring.parse(res.data)
+    return qs.parse(res.data)
+  }
 
-  const cardSeqArry: string[] = result.CardSeq.split('|')
-  const defaultFlagArry: string[] = result.DefaultFlag.split('|')
-  const cardNameArry: string[] = result.CardName.split('|')
-  const cardNoArry: string[] = result.CardNo.split('|')
-  const expireArry: string[] = result.Expire.split('|')
-  const holderNameArry: string[] = result.HolderName.split('|')
-  const deleteFlagArry: string[] = result.DeleteFlag.split('|')
+  public async del(args: IDeleteCardArgs): Promise<IDeleteCardResult> {
+    const res: AxiosResponse = await this.client.post('/payment/DeleteCard.idPass', args)
 
-  return cardSeqArry.map((cardSeq, index) => {
-    return {
-      cardSeq: cardSeq,
-      defaultFlag: defaultFlagArry[index],
-      cardName: cardNameArry[index],
-      cardNo: cardNoArry[index],
-      expire: expireArry[index],
-      holderName: holderNameArry[index],
-      deleteFlag: deleteFlagArry[index]
-    }
-  })
+    return qs.parse(res.data)
+  }
+
+  public async search(args: ISearchCardArgs): Promise<ISearchCardResult[]> {
+    const res: AxiosResponse = await this.client.post('/payment/SearchCard.idPass', args)
+    const result = qs.parse(res.data)
+
+    const cardSeqArry: string[] = result.CardSeq.split('|')
+    const defaultFlagArry: string[] = result.DefaultFlag.split('|')
+    const cardNameArry: string[] = result.CardName.split('|')
+    const cardNoArry: string[] = result.CardNo.split('|')
+    const expireArry: string[] = result.Expire.split('|')
+    const holderNameArry: string[] = result.HolderName.split('|')
+    const deleteFlagArry: string[] = result.DeleteFlag.split('|')
+
+    return cardSeqArry.map((cardSeq, index) => {
+      return {
+        CardSeq: cardSeq,
+        DefaultFlag: defaultFlagArry[index],
+        CardName: cardNameArry[index],
+        CardNo: cardNoArry[index],
+        Expire: expireArry[index],
+        HolderName: holderNameArry[index],
+        DeleteFlag: deleteFlagArry[index]
+      }
+    })
+  }
 }
