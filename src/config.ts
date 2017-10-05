fd0463e71d5bdd5a026f7c612993e15c93a6318f
linyows <linyows@gmail.com>
2017-10-06 02:50:18 +0900

    change config type

--- src/config.ts
+++ src/config.ts
@@ -1,36 +1,47 @@
+import {AxiosRequestConfig} from 'axios'
 import * as process from 'process'
 
 export type TConfig = {
-  endpoint?: string
-  headers?: object
-  timeout?: number
+  axios?: AxiosRequestConfig
   siteID?: string
   sitePass?: string
   shopID?: string
   shopPass?: string
 }
 
+export const defaults: TConfig = {
+  axios: {
+    timeout: 3*60*1000,
+    baseURL: 'https://pt01.mul-pay.jp',
+    headers: {
+      'user-agent': 'GMO PG Client: Unofficial',
+      'content-length': '0',
+    },
+  },
+}
+
 export default class Config {
   static buildByEnv(): TConfig {
     const c: TConfig = {}
+    c.axios = {}
 
     if (process.env.GMOPG_ENDPOINT) {
-      c['endpoint'] = process.env.GMOPG_ENDPOINT
+      c.axios.baseURL = process.env.GMOPG_ENDPOINT
     }
     if (process.env.GMOPG_TIMEOUT) {
-      c['timeout'] = +`${process.env.GMOPG_TIMEOUT}`
+      c.axios.timeout = +`${process.env.GMOPG_TIMEOUT}`
     }
     if (process.env.GMOPG_SITEID) {
-      c['siteID'] = process.env.GMOPG_SITEID
+      c.siteID = process.env.GMOPG_SITEID
     }
     if (process.env.GMOPG_SITEPASS) {
-      c['sitePass'] = process.env.GMOPG_SITEPASS
+      c.sitePass = process.env.GMOPG_SITEPASS
     }
     if (process.env.GMOPG_SHOPID) {
-      c['shopID'] = process.env.GMOPG_SHOPID
+      c.shopID = process.env.GMOPG_SHOPID
     }
     if (process.env.GMOPG_SHOPPASS) {
-      c['shopPass'] = process.env.GMOPG_SHOPPASS
+      c.shopPass = process.env.GMOPG_SHOPPASS
     }
 
     return c
