9a18bd025517361eebf452a9115d3c60874d6896
linyows <linyows@gmail.com>
2017-10-06 10:57:31 +0900

    move to client from util

--- src/client.ts
+++ src/client.ts
@@ -8,3 +8,58 @@ export interface IShopArgs {
   ShopID: string
   ShopPass: string
 }
+
+export enum PayType {
+  Cash = 'Z',
+  Credit = '0',
+  Suica = '1',
+  Edy = '2',
+  Cvs = '3',
+}
+
+export enum Method {
+  Lump = '1',
+  Installment = '2',
+  BonusLump = '3',
+  Revolving = '4',
+  BonusInstallment = '5',
+}
+
+export enum Status {
+  Unprocessed = 'UNPROCESSED',
+  Authenticated = 'AUTHENTICATED',
+  Check = 'CHECK',
+  Capture = 'CAPTURE',
+  Auth = 'AUTH',
+  Sales = 'SALES',
+  Void = 'VOID',
+  Return = 'RETURN',
+  Returnx = 'RETURNX',
+  Sauth = 'SAUTH',
+  Reqsuccess = 'REQSUCCESS',
+  Paysuccess = 'PAYSUCCESS',
+  Payfail = 'PAYFAIL',
+  Expired = 'EXPIRED',
+  Cancel = 'CANCEL',
+}
+
+export enum JobCd {
+  Check = 'CHECK',
+  Capture = 'CAPTURE',
+  Auth = 'AUTH',
+  Sales = 'SALES',
+  Void = 'VOID',
+  Return = 'RETURN',
+  Returnx = 'RETURNX',
+  Sauth = 'SAUTH',
+}
+
+export enum SeqMode {
+  Logic = '0',
+  Physics = '1',
+}
+
+export enum DefaultFlag {
+  BillingObject = '0',
+  NotSubjectToCharge = '1',
+}
