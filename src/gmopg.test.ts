424980f1c832dd7cff0cb652b9db757e4cceb89a
linyows <linyows@gmail.com>
2017-10-07 14:15:24 +0900

    add tests

--- src/gmopg.test.ts
+++ src/gmopg.test.ts
@@ -1,7 +1,17 @@
 import test from 'ava'
 import GMOPG from './gmopg'
 
-test('returns instance', (t) => {
+test('new returns instance', (t) => {
   const pg = new GMOPG({})
   t.is(typeof pg, 'object')
+})
+
+test('.enums returns enum object', (t) => {
+  const pg = new GMOPG({})
+  t.is(typeof pg.enums, 'object')
+})
+
+test('returns generated memberID', (t) => {
+  const ID = GMOPG.generateMemberID('key')
+  t.regex(ID, /key-\w{32}/)
 })
