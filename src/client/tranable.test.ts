d446cbc98a76d2fa417be0edf005d983983d4527
linyows <linyows@gmail.com>
2017-10-11 01:38:26 +0900

    add execTran test

--- src/client/tranable.test.ts
+++ src/client/tranable.test.ts
@@ -48,3 +48,61 @@ test('.entryTran calls API and returns response', async (t) => {
   }
   t.deepEqual(res, expect)
 })
+
+test('.execTran calls API and returns response', async (t) => {
+  t.context.tran.options = {
+    adapter: async (config: AxiosRequestConfig) => {
+      const text = [
+        'Acs=acs',
+        'OrderID=orderid',
+        'Forward=forward',
+        'Method=1',
+        'PayTimes=1',
+        'Approve=approve',
+        'TranID=tranid',
+        'TranDate=trandate',
+        'CheckString=checkstring',
+        'ClientField1=clientfield1',
+        'ClientField2=clientfield2',
+        'ClientField3=clientfield3'
+      ].join('&')
+      const response: AxiosResponse = {
+        data: text,
+        status: 200,
+        statusText: 'OK',
+        headers: {},
+        config
+      }
+
+      return Promise.resolve(response)
+    }
+  }
+
+  const args = {
+    AccessID: 'accessid',
+    AccessPass: 'accesspass',
+    OrderID: 'orderid',
+    Method: '1',
+    PayTimes: 1,
+    CardNo: 'cardno',
+    Expire: 'expire',
+    SecurityCode: '123'
+  }
+  const res = await t.context.tran.execTran(args)
+
+  const expect: IExecTranResult = {
+    Acs: 'acs',
+    OrderID: 'orderid',
+    Forward: 'forward',
+    Method: Method.Lump,
+    PayTimes: '1',
+    Approve: 'approve',
+    TranID: 'tranid',
+    TranDate: 'trandate',
+    CheckString: 'checkstring',
+    ClientField1: 'clientfield1',
+    ClientField2: 'clientfield2',
+    ClientField3: 'clientfield3'
+  }
+  t.deepEqual(res, expect)
+})
