6d0be7d1b317e9786cec5a89f7f6d97e51f2fdc8
linyows <linyows@gmail.com>
2017-10-12 11:45:52 +0900

    add interface for GMOPG

--- src/gmopg.ts
+++ src/gmopg.ts
@@ -8,6 +8,37 @@ import Tranable  from './client/tranable'
 import Config, {TConfig, defaults} from './config'
 import {applyMixins, generateID} from './util'
 
+export interface IGMOPG {
+  config: TConfig
+  client: AxiosInstance
+  options: object
+
+  saveMember: (args: any) => any
+  updateMember: (args: any) => any
+  deleteMember: (args: any) => any
+  searchMember: (args: any) => any
+  defaultMemberData: () => any
+
+  saveCard: (args: any) => any
+  updateCard: (args: any) => any
+  deleteCard: (args: any) => any
+  searchCard: (args: any) => any
+  defaultCardData: () => any
+
+  entryTran: (args: any) => any
+  execTran: (args: any) => any
+  alterTran: (args: any) => any
+  updateTran: (args: any) => any
+  deleteTran: (args: any) => any
+  searchTrade: (args: any) => any
+  changeTran: (args: any) => any
+}
+
+export interface IGMOPGStatic extends IGMOPG {
+  create: (config: TConfig) => IGMOPG
+  generateMemberID: (key: string) => string
+}
+
 export default class GMOPG implements Memberable, Cardable, Tranable {
   public config: TConfig
   public client: AxiosInstance
