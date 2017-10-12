eb631a9dedaadf7dc6960a169b0826b8a7b22fa9
linyows <linyows@gmail.com>
2017-10-12 17:04:28 +0900

    add test for constructor.name

--- src/index.test.ts
+++ src/index.test.ts
@@ -5,3 +5,7 @@ import * as gmopg from './index'
 test('export GMOPG instance as module', (t) => {
   t.true(gmopg instanceof GMOPG)
 })
+
+test('constructor.name returns GMOPG', (t) => {
+  t.is(gmopg.constructor.name, 'GMOPG')
+})
