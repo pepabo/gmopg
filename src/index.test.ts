07179a862023da716b67aaa7425ed0710a1cb35b
linyows <linyows@gmail.com>
2017-10-12 11:50:08 +0900

    to strict

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,7 @@
 import test from 'ava'
+import GMOPG from './gmopg'
 import * as gmopg from './index'
 
 test('export GMOPG instance as module', (t) => {
-  t.true(gmopg instanceof Object)
+  t.true(gmopg instanceof GMOPG)
 })
