a16af5001ab86c989826418fc38df7db940a2de8
linyows <linyows@gmail.com>
2017-10-12 17:01:07 +0900

    skip Fn.name override

--- src/util.ts
+++ src/util.ts
@@ -3,7 +3,9 @@ import {createHash} from 'crypto'
 export function applyMixins(derivedCtor: any, baseCtors: any[]) {
   baseCtors.forEach(baseCtor => {
     Object.getOwnPropertyNames(baseCtor.prototype).forEach(name => {
-      derivedCtor.prototype[name] = baseCtor.prototype[name]
+      if (name !== 'constructor') {
+        derivedCtor.prototype[name] = baseCtor.prototype[name]
+      }
     })
   })
 }
