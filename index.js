9e77840581b81366c833848fbf8af62390935006
linyows <linyows@gmail.com>
2017-10-18 23:58:37 +0900

    update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-module.exports = require('./lib/gmopg');
+"use strict";
+
+const GMOPG = require("./lib/gmopg").default;
+
+// Create the default instance to be exported
+var gmopgInstance = GMOPG.createInstance({});
+
+module.exports = gmopgInstance;
+
+// Allow use of default import syntax in TypeScript
+module.exports.default = gmopgInstance;
