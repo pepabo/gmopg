40063d3d4ac2f20bcdfd8456ce254b1967a49f86
linyows <linyows@gmail.com>
2017-10-19 12:43:20 +0900

    fix for correct types

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import {AxiosInstance} from 'axios'
+import {TConfig} from './config'
 import * as m from './client/memberable.interface'
 import * as c from './client/cardable.interface'
 import * as t from './client/tranable.interface'
-import {TConfig} from './config'
-import {ISiteArgs} from './client.interface'
 import * as enums from './client.enum'
 
 export * from './client/memberable.interface'
@@ -18,23 +17,22 @@ export interface GMOPGInstance {
   client: AxiosInstance
   options: object
   new: (config: TConfig) => GMOPGInstance
-  saveMember: (args: m.ISaveMemberArgs) => m.ISaveMemberResult
-  updateMember: (args: m.IUpdateMemberArgs) => m.IUpdateMemberResult
-  deleteMember: (args: ISiteArgs) => m.IDeleteMemberResult
-  searchMember: (args: ISiteArgs) => m.ISearchMemberResult
-  defaultMemberData: () => any
-  saveCard: (args: c.ISaveCardArgs) => any
-  updateCard: (args: any) => any
-  deleteCard: (args: any) => any
-  searchCard: (args: any) => any
-  defaultCardData: () => any
-  entryTran: (args: t.IEntryTranArgs) => any
-  execTran: (args: any) => any
-  alterTran: (args: any) => any
-  updateTran: (args: any) => any
-  deleteTran: (args: any) => any
-  searchTrade: (args: any) => any
-  changeTran: (args: any) => any
+
+  saveMember: (args: m.ISaveMemberArgs) => Promise<m.ISaveMemberResult>
+  updateMember: (args: m.IUpdateMemberArgs) => Promise<m.IUpdateMemberResult>
+  deleteMember: (args: m.IDeleteMemberArgs) => Promise<m.IDeleteMemberResult>
+  searchMember: (args: m.ISearchMemberArgs) => Promise<m.ISearchMemberResult>
+
+  saveCard: (args: c.ISaveCardArgs) => Promise<c.ISaveCardResult>
+  deleteCard: (args: c.IDeleteCardArgs) => Promise<c.IDeleteCardResult>
+  searchCard: (args: c.ISearchCardArgs) => Promise<c.ISearchCardResult>
+
+  entryTran: (args: t.IEntryTranArgs) => Promise<t.IEntryTranResult>
+  execTran: (args: t.IExecTranArgs) => Promise<t.IExecTranResult>
+  alterTran: (args: t.IAlterTranArgs) => Promise<t.IAlterTranResult>
+  searchTrade: (args: t.ISearchTradeArgs) => Promise<t.ISearchTradeResult>
+  changeTran: (args: t.IChangeTranArgs) => Promise<t.IChangeTranResult>
+
   readonly enums: typeof enums
   createInstance(config: TConfig): GMOPGInstance
   generateMemberID(key: string): string
