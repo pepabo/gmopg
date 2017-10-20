4013d23aa4f4d5e511c30bdcbda7a04928f3072e
linyows <linyows@gmail.com>
2017-10-21 00:44:30 +0900

    add test

--- src/errors.test.ts
+++ src/errors.test.ts
@@ -11,7 +11,9 @@ test('variables is message as instance', (t) => {
 })
 
 test('.parseError returns ErrInfo as array', (t) => {
-  const err = new BadRequest().parseError({ ErrInfo: 'aaa|bbb' })
-  t.is(err.errors[0], 'aaa')
-  t.is(err.errors[1], 'bbb')
+  const err = new BadRequest().parseError({ ErrInfo: 'NC1000009|N0C030G96' })
+  t.is(err.errors[0], '決済NG')
+  t.is(err.errors[1], 'カード会社にて受付を拒否しました。')
+  t.is(err.errInfo[0], 'NC1000009')
+  t.is(err.errInfo[1], 'N0C030G96')
 })
