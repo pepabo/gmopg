ee9e742e8036825b65f22d8227b9c409842eded1
linyows <linyows@gmail.com>
2017-10-23 12:43:16 +0900

    support prototype chain methods as mixin

--- src/util.ts
+++ src/util.ts
@@ -2,6 +2,11 @@ import {createHash} from 'crypto'
 
 export function applyMixins(derivedCtor: any, baseCtors: any[]) {
   baseCtors.forEach((baseCtor) => {
+    Object.getOwnPropertyNames(baseCtor.prototype.__proto__).forEach((name) => {
+      if (name !== 'constructor') {
+        derivedCtor.prototype[name] = baseCtor.prototype[name]
+      }
+    })
     Object.getOwnPropertyNames(baseCtor.prototype).forEach((name) => {
       if (name !== 'constructor') {
         derivedCtor.prototype[name] = baseCtor.prototype[name]
