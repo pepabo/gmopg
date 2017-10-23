48afcaa659f9f6e5c94174633a2e84ba125249c0
linyows <linyows@gmail.com>
2017-10-23 16:38:45 +0900

    add wrapper for axios

--- src/client.ts
+++ src/client.ts
@@ -1,10 +1,28 @@
+import {AxiosInstance, AxiosResponse} from 'axios'
 import {BadRequest} from './errors'
+import * as qs from 'querystring'
 
 export default class Client {
-  public errorHandler(obj: any): void {
-    if (obj !== undefined && obj.ErrCode === undefined) {
-      return
+  public client: AxiosInstance
+  public options: object = {}
+
+  public async post(endpoint: string, data: any): Promise<any> {
+    const res: AxiosResponse = await this.client.post(endpoint, data, this.options)
+    const parsed: any = qs.parse(res.data)
+
+    if (this.isError(parsed)) {
+      const req = {
+        endpoint: endpoint,
+        data: data
+      }
+      throw new BadRequest(`Bad Request: ${endpoint}`).
+        setRequest(req).setResponse(res).parseError(parsed)
     }
-    throw new BadRequest('Error: Bad Request').parseError(obj)
+
+    return parsed
+  }
+
+  public isError(res: any): boolean {
+    return !(res !== undefined && res.ErrCode === undefined)
   }
 }
