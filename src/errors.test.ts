535e54ea950d5b4703e39eed1a167e1c01e42d8e
caub <cyril.auburtin@gmail.com>
2019-11-26 17:41:05 +0100

    Revert to initial errors list

--- src/errors.test.ts
+++ src/errors.test.ts
@@ -13,14 +13,14 @@ test('variables is message as instance', (t) => {
 
 test('.parseError returns ErrInfo as array', (t) => {
   const err = new BadRequest().parseError({ErrInfo: 'NC1000009|N0C030G96'})
-  t.is(err.errors[0].en, 'Input parameter error / billing information presence judgment flag incorrect format')
-  t.is(err.errors[1].en, '[Settlement request] Cancel processing has failed at the subsequent settlement center.')
+  t.is(err.errors[0], '決済NG')
+  t.is(err.errors[1], 'カード会社にて受付を拒否しました。')
   t.is(err.errInfo[0], 'NC1000009')
   t.is(err.errInfo[1], 'N0C030G96')
 })
 
 test('.parseError returns unique ErrInfos', (t) => {
   const err = new BadRequest().parseError({ErrInfo: 'E61030001|E61030001'})
-  t.deepEqual(err.errors, [{ja: 'ご契約内容エラー/現在のご契約では、ご利用になれません。', en: 'Your request cannot be accepted under the current merchant contract.'}])
+  t.deepEqual(err.errors, ['加盟店設定エラー/決済を中止して、問い吅わせにて設定状況を確認して下さい。'])
   t.deepEqual(err.errInfo, ['E61030001'])
 })
--- src/errors.ts
+++ src/errors.ts
@@ -1,12 +1,7 @@
-/**
- * Initial errors (total: 1688 https://github.com/pepabo/gmopg/blob/38d449841513096b40efbfc9c0a89dc8419e382f/src/errors.ts#L20) were added by user: linyows, with Japanese messages
- * We translated them to English with google-translate
- * The error ranges: E00000000-EX1000304, 42C010000-42G990000, M01330001-M01334011, AC2000001-AC2000004 (total: 283) were later added or updated by user: caub, with Japanese and English messages provided by gmo-pg engineers
- */
-const errorDefinition: {[key: string]: {ja: string, en: string}} = require('../error-codes.json');
+const errorDefinition: {[key: string]: string} = require('../error-codes.json');
 
 export class BadRequest extends Error {
-  public errors: {ja: string, en: string}[]
+  public errors: string[]
   public errInfo: string[]
   public response: any
 
