f51ca7fbb362234b8bdc4d3b6ca1bee12b3932ae
tascript <tascript@pepabo.com>
2020-08-27 19:58:41 +0900

    add await

--- src/client/memberable.ts
+++ src/client/memberable.ts
@@ -26,28 +26,28 @@ export default <T extends Constructor<Client>>(Base: T) =>
     }
 
     public async saveMember(args: SaveMemberArgs): Promise<SaveMemberResult> {
-      return this.post<SaveMemberArgs, SaveMemberResult>('/payment/SaveMember.idPass', {
+      return await this.post<SaveMemberArgs, SaveMemberResult>('/payment/SaveMember.idPass', {
         ...this.defaultMemberData(),
         ...args,
       })
     }
 
     public async updateMember(args: UpdateMemberArgs): Promise<UpdateMemberResult> {
-      return this.post<UpdateMemberArgs, UpdateMemberResult>('/payment/UpdateMember.idPass', {
+      return await this.post<UpdateMemberArgs, UpdateMemberResult>('/payment/UpdateMember.idPass', {
         ...this.defaultMemberData(),
         ...args,
       })
     }
 
     public async deleteMember(args: DeleteMemberArgs): Promise<DeleteMemberResult> {
-      return this.post<DeleteMemberArgs, DeleteMemberResult>('/payment/DeleteMember.idPass', {
+      return await this.post<DeleteMemberArgs, DeleteMemberResult>('/payment/DeleteMember.idPass', {
         ...this.defaultMemberData(),
         ...args,
       })
     }
 
     public async searchMember(args: SearchMemberArgs): Promise<SearchMemberResult | null> {
-      return this.post<SearchMemberArgs, SearchMemberResult>('/payment/SearchMember.idPass', {
+      return await this.post<SearchMemberArgs, SearchMemberResult>('/payment/SearchMember.idPass', {
         ...this.defaultMemberData(),
         ...args,
       })
