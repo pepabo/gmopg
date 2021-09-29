9607848a2ed7fa1dd3d2e41e1c7803a466406678
Kohei Futatsuka <futahei1108@icloud.com>
2021-09-29 15:24:12 +0900

    fix

--- src/util.test.ts
+++
@@ -1,6 +0,0 @@
-import test from 'ava'
-import * as Util from './util'
-
-test('.generateID returns key with hash', t => {
-  t.regex(Util.generateID('foo'), /foo-\w{32}/)
-})
--- src/util.ts
+++
@@ -1,7 +0,0 @@
-import { createHash } from 'crypto'
-
-export function generateID(key: string): string {
-  return `${key}-${createHash('md5').update(`${key}-${new Date().toISOString()}`).digest('hex')}`
-}
-
-export type Constructor<T> = new (...args: any[]) => T
